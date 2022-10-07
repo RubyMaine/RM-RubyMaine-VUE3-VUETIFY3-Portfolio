03101a087175b9848066203f59f028e1bd31c6a6
RubyMaine <104354155+RubyMaine@users.noreply.github.com>
2022-10-07 17:55:50 +0500

    MY RUBYMAINE + Vue3 + Vuetify3 ~ Portfolio
    Cайт создано в виде MY RUBYMAINE + Vue3 + Vuetify3 ~ Portfolio личного портфолио RubyMaine.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar app color="black" dark flat class="px-12" >
+      <v-btn >
+        <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon> RubyMaine
+      </v-btn>
+
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')" class="text-yellow"> Главная </v-btn>
+      <v-btn text @click="scroll('about')" > О нас </v-btn>
+      <v-btn text @click="scroll('portfolio')" > Портфолио </v-btn>
+      <v-btn text @click="scroll('services')" > Услуги </v-btn>
+      <v-btn text @click="scroll('page')" > Страница </v-btn>
+      <v-btn text @click="scroll('biog')" > Биог </v-btn>
+      <v-btn text @click="scroll('contact')" > Контакт </v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,352 @@
+<template>
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative" class="mt-16">
+              <h1 class="text-grey"> Добрый день, </h1>
+              <h1 class="text-white"> Я-RubyMaine </h1>
+              <span class="text-grey"> Front-End & Back-End + Mobile & Разработчик </span><br />
+
+              <v-btn tile dark class="text-yellow mt-8" variant="outlined"> Свяжитесь со мной </v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div
+              style="
+                position: absolute;
+                z-index: 9999;
+                bottom: 0;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              "
+              class="mt-16"
+            >
+              <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <div style="position: relative; z-index: 9999" class="mt-16">
+              <v-img src="i1.png" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="i2.png" max-height="300" class=""></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16"> Обо мне </h5>
+              <div style="width: 120px">
+                <v-slider
+                  v-model="slider2"
+                  color="yellow"
+                  label="track-color"
+                ></v-slider>
+              </div>
+              <h4 class="mt-n4"> Я креативный веб Front-End & Back-End + Mobile </h4>
+              <h4 class=""> Разработчик</h4>
+              <p class="text-grey">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aspernatur, ullam perspiciatis fugiat temporibus laboriosam
+                accusamus tempora repellendus sequi sed cum dolorum vero est.
+                Placeat dicta architecto reiciendis est veniam. Unde.
+              </p>
+              <br />
+              <p class="text-grey">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aspernatur, ullam perspiciatis fugiat temporibus laboriosam
+                accusamus tempora repellendus sequi sed cum dolorum vero est.
+                Placeat dicta architecto reiciendis est veniam. Unde.
+              </p>
+              <v-btn tile dark color="yellow" class="mt-4">
+                Скачать мой резюме
+              </v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <div class="text-center mt-4">
+        <h2> Что я делаю </h2>
+        <div style="width: 120px; margin: 0 auto">
+          <v-slider
+            v-model="slider2"
+            color="yellow"
+            label="track-color"
+          ></v-slider>
+        </div>
+      </div>
+      <v-col cols="12" class="padd" id="portfolio">
+        <div class="first" id="project">
+          <v-row>
+            <v-col cols="12">
+              <div class="child">
+                <v-btn
+                  icon="fas fa-laptop"
+                  color="#FBDF7E"
+                  class="text-white"
+                ></v-btn>
+                <h3 class="ml-3 mt-4"> Front-End & Back-End </h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+              <div class="child">
+                <v-btn
+                  icon="fas fa-mobile-alt"
+                  color="#FBDF7E"
+                  class="text-white"
+                ></v-btn>
+                <h3 class="ml-3 mt-4"> Mobile </h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+              <div class="child">
+                <v-btn
+                  icon="fas fa-camera"
+                  color="#FBDF7E"
+                  class="text-white"
+                ></v-btn>
+                <h3 class="ml-3 mt-4"> Креативный дизайна </h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
+                  <br />consectetur adipis
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" id="services">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mr-2"> Все </v-btn>
+          <v-btn class="mr-2" variant="tonal"> FONT-END </v-btn>
+          <v-btn class="mr-2" variant="tonal"> BACK-END </v-btn>
+          <v-btn class="mr-2" variant="tonal"> МОБИЛЬНЫЙ-APP </v-btn>
+          <v-btn variant="tonal"> Дизайины </v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" class="imgHover">
+        <v-row class="fill-height" align="center" justify="center">
+          <template v-for="(item, i) in items" :key="i">
+            <v-col cols="12" md="4">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card
+                  :elevation="isHovering ? 12 : 2"
+                  :class="{ 'on-hover': isHovering }"
+                  v-bind="props"
+                >
+                  <v-img :src="item.img" height="225px" cover> </v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+      <v-col cols="12" sm="12">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mt-4"> Загрузи еще </v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" class="px-16" id="contact">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <div class="child">
+              <h1>Контактная информация</h1>
+              <v-btn
+                icon="fas fa-map-marker-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption"> г.Ташкент, Яшнабадский район </span
+              ><br />
+              <v-btn
+                icon="fas fa-phone-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption"> +99894 662 15 51 </span> <br />
+              <v-btn
+                icon="fas fa-envelope"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption"> rubymaine@rubymaine.uz </span> <br />
+            </div>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h1 class="mt-8"> Отправить сообщение </h1>
+            <v-divider></v-divider>
+            <span class="text-caption"
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              fugiat officia, odio eaque exercitationem libero nesciunt placeat,
+              repellat obcaecati sed tenetur! Est labore aliquam amet
+              consequatur necessitatibus fugit obcaecati facilis!</span
+            >
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Ваш имя:"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Телефон номер:"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea
+              label="Сообщение"
+              persistent-hint
+              variant="outlined"
+            ></v-textarea>
+            <v-btn color="#FBDF7E" class="mt-2"> Отправить </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-container>
+    <FooterView />
+  </v-app>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import NavBar from "../components/NavBar.vue";
+import FooterView from "../components/FooterView.vue";
+
+// Components
+//import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: "HomeView",
+  setup() {
+    return {
+      slider2: 50,
+
+      items: [
+        {
+          img: "i3.jpg",
+        },
+        {
+          img: "i4.jpg",
+        },
+        {
+          img: "i5.jpg",
+        },
+        {
+          img: "i6.jpeg",
+        },
+        {
+          img: "i9.jpg",
+        },
+        {
+          img: "i8.jpg",
+        },
+      ],
+    };
+  },
+  components: {
+    NavBar,
+    FooterView
+},
+});
+</script>
+<style scoped>
+.v-container {
+  padding: 16px 0 16px 0;
+}
+
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+.head:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #fbdf7e;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+.first {
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: center;
+  margin-right: 8px;
+}
+.imgHover {
+  padding: 0 200px;
+}
+.pre {
+  width: 100%;
+  height: 380px;
+  text-align: center;
+  padding: 0 200px;
+  background-color: #f5f5f5;
+}
+.hire {
+  width: 100%;
+  height: 200px;
+  padding: 0 200px;
+  background-color: #e9e9e9;
+  margin-top: -24px;
+}
+
+</style>
